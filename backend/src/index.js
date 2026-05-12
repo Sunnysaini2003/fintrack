ec56cbc4445a57e3f0ea2285bc367ae4902e8686
Sunnysaini2003 <sunnysaini.ldh@gmail.com>
2026-05-12 16:04:21 +0530

    feat:cors update for frontend

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,7 +23,7 @@ const PORT = process.env.PORT || 5000;
 // Core middleware
 app.use(
   cors({
-    origin: "http://localhost:5000",
+    origin: "http://localhost:5173",
     credentials: true,
   })
 );
